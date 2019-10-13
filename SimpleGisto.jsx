bc5df1db27660ae7590ce4fa5dde164eb3c57775
alexey@barykin.com <lpY6YIYP2DXe>
2019-10-14 01:47:58 +0300

    Beta release

---
+++ SimpleGisto.jsx
@@ -0,0 +1,34 @@
+class SimpleGistoItem extends React.Component {
+	render() {
+		return (
+			<li style={this.props.style}>{this.props.index}</li>
+		)
+	}
+}
+
+class SimpleGisto extends React.Component {
+	constructor() {
+		super();
+		window.addEventListener("resize", this.render);
+	}
+
+	render() {
+		let max = 0;
+		Object.keys(this.props.data).map(index => {
+			if (max < this.props.data[index]) {
+				max = this.props.data[index]
+			}
+		});
+		let content = Object.keys(this.props.data).map(index => {
+			let value = this.props.data[index],
+				width = value / max * 100,
+				style = {width: width + '%'};
+
+			return (
+				<SimpleGistoItem style={style} index={index} value={value}/>
+			);
+		});
+
+		return <ul>{content}</ul>;
+	}
+}
